b00dfee233c4b530751f5904914dd2e510905a28
dayyan49 <165640413+dayyan49@users.noreply.github.com>
2025-03-21 22:33:05 +0530

    Delete Book.java

--- Book.java
+++
@@ -1,9 +0,0 @@
-class Book extends BookBase{
-    public Book(String title,String author,String ISBN,int copies) {
-        super(title,author,ISBN,copies);
-    }
-    @Override
-    public void displayDetails(){
-        System.out.println("Title: "+title+", Author: "+author+", ISBN: "+ISBN+", Copies: "+availableCopies);
-    }
-}
